2a7a0f4ab9fd69b2ff2d3d807967abb5c3c11d6e
B0user <56859914+B0user@users.noreply.github.com>
2022-08-28 11:52:47 +0600

    Browser vs Mobile added

--- src/features/modelview/ModelView.js
+++ src/features/modelview/ModelView.js
@@ -6,6 +6,8 @@ import "../../mv.css";
 import { useState} from "react";
 import { RWebShare } from 'react-web-share';
 import { API_URL } from "../../config";
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import QRCode from "../qrcodes/QRCode";
 
 const ModelView = () => {
   const navigate = useNavigate();
@@ -58,105 +60,112 @@ const ModelView = () => {
     const found = data.find((model) => { return model.color === color && model.size === size; });
     if (!found) return( <p>Модель не найдена, ваш URL был изменен</p>);  
     return ( 
-    <model-viewer
-      src={API_URL+found.model}
-      alt="Couch"
-      ar
-      ar-modes="webxr quick-look"
-      environment-image="neutral"
-      auto-rotate
-      camera-controls
-      camera-orbit="90deg 50deg 2m"
-      ar-scale="fixed"
-    >
-      <nav className="navbar">
-        <div className="container-fluid d-flex justify-content-between">
-          <svg
-            className="back"
-            onClick={() => navigate(-1)}
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            viewBox="0 0 320 512"
+      <>
+        <MobileView>
+          <model-viewer
+            src={API_URL+found.model}
+            alt="Couch"
+            ar
+            ar-modes="webxr scene-viewer quick-look"
+            environment-image="neutral"
+            auto-rotate
+            camera-controls
+            camera-orbit="90deg 50deg 2m"
           >
-            <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
-          </svg>
-          <RWebShare
-            data = {{
-              title: 'BritishAsia Home',
-              text: '[BritishAsia Home] Примерьте ковер не выходя из дома!',
-              url: window.location.href
-            }}
-            onClick={() => console.log("onShare successfully!")}
-          >
-          <svg
-            className="share"
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            viewBox="0 0 512 512"
-          >
-            <path d="M503.7 226.2l-176 151.1c-15.38 13.3-39.69 2.545-39.69-18.16V272.1C132.9 274.3 66.06 312.8 111.4 457.8c5.031 16.09-14.41 28.56-28.06 18.62C39.59 444.6 0 383.8 0 322.3c0-152.2 127.4-184.4 288-186.3V56.02c0-20.67 24.28-31.46 39.69-18.16l176 151.1C514.8 199.4 514.8 216.6 503.7 226.2z" />
-          </svg>
-          </RWebShare>
-        </div>
-      </nav>
-      <button slot="ar-button" id="ar-button">
-        Посмотреть у себя
-      </button>
-      <div className="container-fluid fixed-bottom py-2 px-0 d-flex justify-content-center align-items-center flex-column bg-light">
-        <button
-          data-bs-toggle="collapse"
-          data-bs-target="#info"
-          className="btn btn-primary"
-        >
-          ВВЕРХ
-        </button>
-        <div className="collapse" id="info">
-          {data?.length ? (
-            <form onSubmit={() => handleSubmit()}>
-              <label htmlFor="color">Выберите цвет:</label>
-              <select
-                name="color"
-                id="color"
-                value={color}
-                onChange={(e) => onColorChange(e)}
-                required
+            <nav className="navbar">
+              <div className="container-fluid d-flex justify-content-between">
+                <svg
+                  className="back"
+                  onClick={() => navigate(-1)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+                </svg>
+                <RWebShare
+                  data = {{
+                    title: 'BritishAsia Home',
+                    text: '[BritishAsia Home] Примерьте ковер не выходя из дома!',
+                    url: window.location.href
+                  }}
+                  onClick={() => console.log("onShare successfully!")}
+                >
+                <svg
+                  className="share"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M503.7 226.2l-176 151.1c-15.38 13.3-39.69 2.545-39.69-18.16V272.1C132.9 274.3 66.06 312.8 111.4 457.8c5.031 16.09-14.41 28.56-28.06 18.62C39.59 444.6 0 383.8 0 322.3c0-152.2 127.4-184.4 288-186.3V56.02c0-20.67 24.28-31.46 39.69-18.16l176 151.1C514.8 199.4 514.8 216.6 503.7 226.2z" />
+                </svg>
+                </RWebShare>
+              </div>
+            </nav>
+            <button slot="ar-button" id="ar-button">
+              Посмотреть у себя
+            </button>
+            <div className="container-fluid fixed-bottom py-2 px-0 d-flex justify-content-center align-items-center flex-column bg-light">
+              <button
+                data-bs-toggle="collapse"
+                data-bs-target="#info"
+                className="btn btn-primary"
               >
-                {uniqueColors.map((color, i) => (
-                  <option key={i} value={color}>
-                    {color}
-                  </option>
-                ))}
-              </select>
-              <label htmlFor="size">Выберите размер:</label>
-              <select
-                name="size"
-                id="size"
-                value={size}
-                onChange={(e) => setSize(e.target.value)}
-                required
-              >
-                {data.map(
-                  (model, i) =>
-                    model.color === color && (
-                      <option key={i} value={model.size}>
-                        {model.size}
-                      </option>
-                    )
+                ВВЕРХ
+              </button>
+              <div className="collapse" id="info">
+                {data?.length ? (
+                  <form onSubmit={() => handleSubmit()}>
+                    <label htmlFor="color">Выберите цвет:</label>
+                    <select
+                      name="color"
+                      id="color"
+                      value={color}
+                      onChange={(e) => onColorChange(e)}
+                      required
+                    >
+                      {uniqueColors.map((color, i) => (
+                        <option key={i} value={color}>
+                          {color}
+                        </option>
+                      ))}
+                    </select>
+                    <label htmlFor="size">Выберите размер:</label>
+                    <select
+                      name="size"
+                      id="size"
+                      value={size}
+                      onChange={(e) => setSize(e.target.value)}
+                      required
+                    >
+                      {data.map(
+                        (model, i) =>
+                          model.color === color && (
+                            <option key={i} value={model.size}>
+                              {model.size}
+                            </option>
+                          )
+                      )}
+                    </select>
+                    <br />
+                    <button className="btn btn-success">Применить настройки</button>
+                  </form>
+                ) : (
+                  <p>Нет вариаций</p>
                 )}
-              </select>
-              <br />
-              <button className="btn btn-success">Применить настройки</button>
-            </form>
-          ) : (
-            <p>Нет вариаций</p>
-          )}
-        </div>
-      </div>
-    </model-viewer>
+              </div>
+            </div>
+          </model-viewer>
+        </MobileView>
+        <BrowserView>
+          <QRCode url={window.location.href} isImage={true} isButton={false}/>
+        </BrowserView>
+      </>
+    
     );  
   }
 };
