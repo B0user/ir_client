2a87e1558949f016e6bfed33793bae59a36d979a
B0user <56859914+B0user@users.noreply.github.com>
2022-09-03 23:02:46 +0600

    ModelView style changes

--- src/features/modelview/ModelView.js
+++ src/features/modelview/ModelView.js
@@ -132,9 +132,11 @@ const ModelView = () => {
                   <path d="M503.7 226.2l-176 151.1c-15.38 13.3-39.69 2.545-39.69-18.16V272.1C132.9 274.3 66.06 312.8 111.4 457.8c5.031 16.09-14.41 28.56-28.06 18.62C39.59 444.6 0 383.8 0 322.3c0-152.2 127.4-184.4 288-186.3V56.02c0-20.67 24.28-31.46 39.69-18.16l176 151.1C514.8 199.4 514.8 216.6 503.7 226.2z" />
                 </svg>
                 </RWebShare>
+                <button className="btn rounded-pill btn-outline-secondary w-75" 
+                  onClick={() => navigate(`/modelview/${client_id}`)}>Каталог</button>
                 <svg
                   className="back"
-                  onClick={() => navigate(-1)}
+                  onClick={() => navigate(`/modelview/${client_id}`)}
                   xmlns="http://www.w3.org/2000/svg"
                   width="1em"
                   height="1em"
@@ -152,9 +154,9 @@ const ModelView = () => {
               <button
                 data-bs-toggle="collapse"
                 data-bs-target="#settings"
-                className="btn btn-primary btn-up"
+                className="btn btn-primary rounded-0 rounded-top"
               >
-                <FontAwesomeIcon icon={faAngleUp}/>
+                ПОМЕНЯТЬ
               </button>
               <div className="collapse p-2 container-fluid text-center bg-cp-clouds" id="settings">
                 <h3>Настройки:</h3>
