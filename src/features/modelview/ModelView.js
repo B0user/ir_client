664339ba1a3a814b4eac4f0ee2585bc139637f68
B0user <site88sonic.ammm@gmail.com>
2022-11-17 20:38:16 +0600

    AR permission request disabled

--- src/features/modelview/ModelView.js
+++ src/features/modelview/ModelView.js
@@ -98,15 +98,16 @@ const ModelView = () => {
     navigate(`?size=${size}`);
     searchParams.set("size", size);
   };
-  const requestARAccess = () => {
-    navigator.getUserMedia(
-      { augmentedReality: true },
-      function (stream) {
-        stream.getTracks().forEach((x) => x.stop());
-      },
-      (err) => console.log(err)
-    );
-  };
+
+  // const requestARAccess = () => {
+  //   navigator.getUserMedia(
+  //     { augmentedReality: true },
+  //     function (stream) {
+  //       stream.getTracks().forEach((x) => x.stop());
+  //     },
+  //     (err) => console.log(err)
+  //   );
+  // };
 
   if (isProductsFetchError)
     return (
@@ -169,17 +170,6 @@ const ModelView = () => {
                   >
                     Подробнее
                   </a>
-                  {/* <svg
-                    className="back"
-                    onClick={() => navigate(`/modelview/${client_id}`)}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1em"
-                    height="1em"
-                    fill="currentColor"
-                    viewBox="0 0 320 512"
-                  >
-                    <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
-                  </svg> */}
                   <FontAwesomeIcon
                     icon={faQuestion}
                     onClick={() => setTutorialPopupActive(true)}
@@ -205,7 +195,6 @@ const ModelView = () => {
               <button
                 slot="ar-button"
                 id="ar-button"
-                onClick={requestARAccess}
               >
                 Посмотреть у себя
               </button>
