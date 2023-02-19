a50b40dce64cbafa3f6a5667ea9b3483a7d4c226
B0user <site88sonic.ammm@gmail.com>
2023-02-19 18:32:34 +0600

    darker BG

--- src/components/Home.js
+++ src/components/Home.js
@@ -146,7 +146,7 @@ const Home = () => {
       {/* Hero Section */}
   <section
     className="d-flex align-items-center position-relative"
-    style={{ height: "100vh" }}
+    style={{ backgroundСolor: rgba(0, 0, 0, 0.65), height: "100vh" }}
   >
     {/* <video autoPlay loop muted>
       <source src="/landing/video/video.webm" type="video/webm" />
