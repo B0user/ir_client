2d4cb910834670316e5ed848436fe8571986cab3
B0user <site88sonic.ammm@gmail.com>
2023-04-08 14:16:51 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home/>} />
-      {/* demo */}
-      <Route path="/demo" element={<DemoCatalog/>} />
-      <Route path="/demo/:product_id" element={<DemoMV/>} />
       <Route path="/" element={<Layout />}>
+        {/* demo */}
+        <Route path="demo" element={<DemoCatalog/>} />
+        <Route path="demo/:product_id" element={<DemoMV/>} />
         {/* public routes */}
         <Route path="modelview/:client_id" element={<BrandFittingRoom/>} />
         <Route path="modelview/:client_id/:product_id" element={<ModelView/>} />
