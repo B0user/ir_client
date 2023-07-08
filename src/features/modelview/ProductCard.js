02ce8c2386a21ddcbfddcac168300a8c7ecefe54
B0user <site88sonic.ammm@gmail.com>
2023-07-08 15:00:56 +0600

    changes

--- src/features/modelview/ProductCard.js
+++ src/features/modelview/ProductCard.js
@@ -11,19 +11,32 @@ import { faXmark, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import "./productcard.css";
 import { API_URL } from "../../config";
 
-const DetailsThumb = ({ images }) => {
+const DetailsThumb = ({ images, setSelectedImage }) => {
     if (!Array.isArray(images)) {
-        // If images is not an array, you can return null or handle it in a different way
-        return null;
-      }
+      // If images is not an array, you can return null or handle it in a different way
+      return null;
+    }
+  
+    const handleImageClick = (image) => {
+      setSelectedImage(image);
+    };
+  
     return (
-        <div className="images">
+      <div className="images">
         {images.map((img, index) => (
-            <img crossOrigin="anonymous" src={API_URL + img} alt="" key={index} height="100" />
+          <img
+            crossOrigin="anonymous"
+            src={API_URL + img}
+            alt=""
+            key={index}
+            height="100"
+            onClick={() => handleImageClick(img)}
+          />
         ))}
-        </div>
+      </div>
     );
-};
+  };
+  
 
 const fetchModelInfo = (product_id) => {
     return axios.get(`/mv/exactproduct/${product_id}`);
@@ -33,6 +46,7 @@ const ProductCard = () => {
     const navigate = useNavigate();
     // URL data
     const { product_id } = useParams();
+    const [selectedImage, setSelectedImage] = useState(product?.thumb_path);
 
     const {isLoading,
         isSuccess,
@@ -41,17 +55,17 @@ const ProductCard = () => {
     const product= result?.data;
     if(isLoading) return <p>Loading...</p>
     if(isSuccess && product) return (
-        <div className="details vh-100" key={product?._id}>
+        <div className="align-items-baseline details vh-100" key={product?._id}>
             <FontAwesomeIcon icon={faAngleLeft}  onClick={() => navigate(-1)}/>
-            <h2>{product?.name}</h2>
+            <h2 className='w-75'>{product?.name}</h2>
             <FontAwesomeIcon icon={faXmark}  onClick={() => navigate(-1)}/>
             
             <div className="big-img">
-                <img crossOrigin="anonymous" src={API_URL + product?.thumb_path} alt="" />
+                <img crossOrigin="anonymous" src={API_URL + selectedImage} alt="" />
             </div>
             
             <div className="mx-4">
-                <DetailsThumb images={product?.image_paths? product.image_paths: ''} />
+                <DetailsThumb images={product?.image_paths? product.image_paths: ''} setSelectedImage={setSelectedImage}/>
         
             
                 <div className="row">
